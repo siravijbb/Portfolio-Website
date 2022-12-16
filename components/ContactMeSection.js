79dbf80cffdf9a781500c328ec2166627168ee0a
Siravijbb <siravijbb@gmail.com>
2022-12-16 08:38:07 +0700

    1

--- components/ContactMeSection.js
+++ components/ContactMeSection.js
@@ -4,32 +4,9 @@ import ContactMeForms from './ContactMeForms'
 
 export default function ContactMeSection(){
 
-    const [contactClasses,setContactClasses] = useState('p-0 mt-7 grid mx-auto w-11/12 xl:w-4/6 invisible')
-
-    useEffect(() => {
-        const sectionOneOptions = {
-            rootMargin: "0px 0px -200px 0px"
-        };
-
-        const sectionTwoObserver = new IntersectionObserver(function (
-                entries,
-                sectionTwoObserver
-            ) {
-                entries.forEach(entry => {
-                    if (entry.isIntersecting) {
-                        setContactClasses('p-0 mt-7 grid mx-auto w-11/12 xl:w-4/6 visible animate__animated animate__fadeInUp')
-                    }
-                });
-            },
-            sectionOneOptions);
-
-        sectionTwoObserver.observe(document.getElementById("ContactMe"));
-    })
+   
 
     return(
-        <div id="ContactMe" className={contactClasses}>
-            <Title mainTitle="Contact Me" secondaryTitle="Discuss a project with me !" />
-            <ContactMeForms />
-        </div>
+        <div id="ContactMe" className="p-0 mt-7 grid mx-auto w-11/12 x1:w-4/6">
     )
 }
